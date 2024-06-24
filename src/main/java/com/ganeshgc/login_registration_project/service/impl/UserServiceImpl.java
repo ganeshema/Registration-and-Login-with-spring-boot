2a40ca34f72b9d5c9a40ab7d953de053bda49e33
Ganesh GC <ganeshlema2021@gmail.com>
2024-06-24 09:44:31 -0500

    spring security configuration

--- src/main/java/com/ganeshgc/login_registration_project/service/impl/UserServiceImpl.java
+++ src/main/java/com/ganeshgc/login_registration_project/service/impl/UserServiceImpl.java
@@ -1,15 +1,28 @@
 package com.ganeshgc.login_registration_project.service.impl;
 
+import com.ganeshgc.login_registration_project.model.Role;
 import com.ganeshgc.login_registration_project.model.User;
 import com.ganeshgc.login_registration_project.repository.UserRepository;
 import com.ganeshgc.login_registration_project.service.UserService;
 import com.ganeshgc.login_registration_project.web.Dto.UserRegistrationDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     private UserRepository userRepository;
     @Override
@@ -18,5 +31,18 @@ public class UserServiceImpl implements UserService {
         BeanUtils.copyProperties(userRegistrationDto, user);
         userRepository.save(user);
         return userRegistrationDto;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       User user=userRepository.findByEmail(username);
+       if(user==null){
+           throw new UsernameNotFoundException("Invalid username or password.");
+       }
+       return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),mapRolesToAuthorities(user.getRoles()));
+    }
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+
     }
 }
